2dd5a020b5bace77cc1771e3aaa04fd112da74f5
eras0r <er4s0r@gmail.com>
2015-08-04 08:04:40 +0200

    code cleanup of users routes

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var _ = require('lodash');
 var User = require('../models/User');
 var Role = require('../models/Role');
 
-/* GET /characters listing. */
+/* GET /users listing. */
 router.get('/', function (req, res, next) {
     // exclude roles.users from selection
     User.find().populate({path: 'roles', select: '-users'}).exec(function (err, users) {
@@ -34,66 +34,14 @@ router.post('/', function (req, res, next) {
 
                 // add the newly created user to the role.users list for bidirectional linking
                 role.users.push(user);
-                //role.rolename += '1';
                 role.save();
             });
         });
 
         res.status(201);
-        res.location('/characters/' + user._id);
+        res.location('/users/' + user._id);
         res.json(user);
     });
-
-    //////////////////////////////////
-    //var user = new User(req.body);
-
-    //////////////////////////////////
-    //var user = new User();
-    //
-    //user.username = req.body.username;
-    //user.algorithm = req.body.algorithm;
-    //user.iterations = req.body.iterations;
-    //user.salt = req.body.salt;
-    //user.password = req.body.password;
-    //
-    //console.log('user.id = ', user._id);
-    //console.log('user.roles = ', user.roles);
-    //
-    //
-    //_.forEach(req.body.roles, function (roleId) {
-    //    console.log('role id = ', roleId);
-    //
-    //    //var role = new Role({});
-    //    //role.users.push(user);
-    //    //role.save();
-    //    //
-    //    //console.log('saved role: ', role);
-    //
-    //    Role.findById(roleId, function (err, role) {
-    //        console.log('role = ', role);
-    //        if (err) {
-    //            return next(err);
-    //        }
-    //
-    //        // add the newly created user to the role.users list for bidirectional linking
-    //        //role.users.push(user);
-    //        //role.rolename += '1';
-    //        //role.save();
-    //
-    //        user.roles.push(role);
-    //
-    //        //console.log('saved role: ', role);
-    //
-    //        console.log('user.roles = ', user.roles);
-    //        console.log('=== saving user ===');
-    //        user.save();
-    //
-    //        res.status(201);
-    //        res.location('/users/' + user._id);
-    //        res.json(user);
-    //    });
-    //});
-    //////////////////////////////////
 
 });
 
